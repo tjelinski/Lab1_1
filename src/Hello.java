551fa1bc2dfc1c53e1edd43789cc879b08de9c10
t.jelinski <tjelinski@student.wsb-nlu.edu.pl>
2023-10-28 01:27:26 +0200

    modifying the hello class to print the student's main information

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,13 @@
 // tjelinski@student.wsb-nlu.edu.pl
 public class Hello {
     public static void main(String[] args) {
+        String nameSurname = "Tomasz Jeliński";
+        String university = "WSB-NLU";
+        String fieldOfStudy = "Computer Science";
 
-        System.out.println("Hello world");
+        System.out.println("Name and Surname: " + nameSurname);
+        System.out.println("Field of study: " + fieldOfStudy);
+        System.out.println("University: " + university);
 
     }
 }
