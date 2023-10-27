f8cf4959b3526e417d635332f0f53e940ee5e77e
t.jelinski <tjelinski@student.wsb-nlu.edu.pl>
2023-10-28 01:46:50 +0200

    modifying the hello class to print the student's main information, user asked for input information from keyboard

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,19 @@
 // tjelinski@student.wsb-nlu.edu.pl
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        String nameSurname = "Tomasz Jeliński";
-        String university = "WSB-NLU";
-        String fieldOfStudy = "Computer Science";
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter main information about the student: ");
+        System.out.println("Name and surname");
+        String nameSurname = scanner.nextLine();
+        System.out.println("University");
+        String university = scanner.nextLine();
+        System.out.println("Field of Study");
+        String fieldOfStudy = scanner.nextLine();
+
+        System.out.println("Information about student");
         System.out.println("Name and Surname: " + nameSurname);
         System.out.println("Field of study: " + fieldOfStudy);
         System.out.println("University: " + university);
