42f839fabfbf31f6cb6fd8fcc42f7a4d7153a57e
t.jelinski <tjelinski@student.wsb-nlu.edu.pl>
2023-10-28 10:03:14 +0200

    add short comments to code lines

--- src/Hello.java
+++ src/Hello.java
@@ -1,23 +1,23 @@
 // tjelinski@student.wsb-nlu.edu.pl
-import java.util.Scanner;
+import java.util.Scanner;       //biblioteka java umożliwiająca wprowadzanie danych z klawiatury przez użytkownika [zawarta w pakiecie java.util]
 
-public class Hello {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Hello {        //publiczna klasa main [dostępna dla wszystkich]
+    public static void main(String[] args) {        //publiczna statyczna metoda main
+        Scanner scanner = new Scanner(System.in);   //obiekt Scanner o nazwie scanner do wprowadzania danych
 
-        System.out.println("Enter main information about the student: ");
+        System.out.println("Enter main information about the student: ");   //blok 'zbierajacy' dane o osobie; System.out.println wyświetla żadane do wpr. dane
         System.out.println("Name and surname");
-        String nameSurname = scanner.nextLine();
+        String nameSurname = scanner.nextLine();    // 'zbiera' dane wpr. przez użytkownika i przypisuje do zmiennej
         System.out.println("University");
         String university = scanner.nextLine();
         System.out.println("Field of Study");
         String fieldOfStudy = scanner.nextLine();
 
-        System.out.println("Information about student");
+        System.out.println("Information about student"); // 'wyświetlenie' zebranych danych
         System.out.println("Name and Surname: " + nameSurname);
         System.out.println("Field of study: " + fieldOfStudy);
         System.out.println("University: " + university);
 
-        scanner.close();
+        scanner.close();    // 'zamknięcie' obiektu Scanner, zakończenie wpr. danych i zwolnienie zasobów
     }
 }
